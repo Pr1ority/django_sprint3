3aa546b2427dc776c756c8af3a517ba9a4e958d3
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-03-27 13:20:43 +0300

    Update

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 from blog.models import Post, Category
 
 
-def get_published_posts(posts_queryset):
-    return posts_queryset.filter(is_published=True,
-                                 pub_date__lte=timezone.now(),
-                                 category__is_published=True)
+def get_published_posts(posts):
+    return posts.filter(is_published=True,
+                        pub_date__lte=timezone.now(),
+                        category__is_published=True)
 
 
 def index(request):
@@ -30,7 +30,7 @@ def category_posts(request, category_slug):
     template_name = 'blog/category.html'
     category = get_object_or_404(Category, slug=category_slug,
                                  is_published=True)
-    all_posts = Post.objects.all()
-    post_list = get_published_posts(all_posts).filter(category=category)
+    all_posts = Post.objects.filter(category=category)
+    post_list = get_published_posts(all_posts)
     context = {'category': category, 'post_list': post_list}
     return render(request, template_name, context)
